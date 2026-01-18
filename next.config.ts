ac0b828788c82547f5a8aaa9e4bfe3d89c15ccfd
안태현 <dksxogus1012@gmail.com>
2026-01-18 20:36:41 +0900

    :wrench: next-pwa 설치 및 prettier 설정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default pwaConfig(nextConfig);
